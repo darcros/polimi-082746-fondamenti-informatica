66fa0999d9efe200054694d0159bb56e1061f88f
darcros <darcros7@gmail.com>
2021-11-30 23:33:06 +0100

    compila con ottimizzazioni

--- esercizi/numeri-amicabili-speed/benchmark.sh
+++ esercizi/numeri-amicabili-speed/benchmark.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# sposta la workdir nella cartella dove sta questo script e compila il programma
+# sposta la workdir nella cartella dove sta questo script
 cd "$(dirname "$0")"
-make build
+
+# cancella il vecchio eseguibile
+make clean
+# compila il programma con le ottimizzazioni attivate, che alla fine fanno poco differenza
+make CLFLAGS=-O3 build
 
 # array dei numeri da provare per misurare il tempo
 numeri=(
@@ -24,7 +28,9 @@ for n in ${numeri[@]}; do
   printf "%10s: %20s %20s\n" "$n" "$coppie" "$tempo"
 done
 
-# Con il mio PC con Intel Core i3-3240T CPU @ 2.90GHz
+# CPU:      Intel Core i3-3240T @ 2.90GHz
+# compiler: gcc (Ubuntu 10.3.0-1ubuntu1) 10.3.0
+# senza specificare ottimizzazioni
 #
 #    100000:    13 coppie trovate     0.006612 secondi
 #   1000000:    40 coppie trovate     0.159447 secondi
@@ -35,3 +41,17 @@ done
 # 100000000:   231 coppie trovate    32.957926 secondi
 # 200000000:   300 coppie trovate    81.357181 secondi
 # 300000000:   352 coppie trovate   113.590962 secondi
+
+# CPU:      Intel Core i3-3240T @ 2.90GHz
+# compiler: gcc (Ubuntu 10.3.0-1ubuntu1) 10.3.0
+# con ottimizzazioni attivate (flat -O3)
+#
+#    100000:    13 coppie trovate     0.006880 secondi
+#   1000000:    40 coppie trovate     0.141177 secondi
+#   5000000:    71 coppie trovate     1.138773 secondi
+#  10000000:   100 coppie trovate     2.546908 secondi
+#  25000000:   148 coppie trovate     6.640592 secondi
+#  50000000:   189 coppie trovate    13.888317 secondi
+# 100000000:   231 coppie trovate    28.568813 secondi
+# 200000000:   300 coppie trovate    62.896747 secondi
+# 300000000:   352 coppie trovate   107.628434 secondi
